8e83c1ff6af00eb1698ddf57dcd02035ec689486
baggytk <miraclerowlandgodspower@gmail.com>
2026-09-14 22:00:49 +0100

    Update API_BASE to use window.location.origin

--- config.js
+++ config.js
@@ -15,4 +15,4 @@
 // Every fetch() call in script.js and checkout.html uses this
 // value instead of a hardcoded localhost address.
 
-const API_BASE = "http://localhost:3000";
+const API_BASE = window.location.origin;
